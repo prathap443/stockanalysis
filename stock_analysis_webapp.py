71ec58d746883e656dca404fbb7b93030854c3d2
prathap443 <40711874+prathap443@users.noreply.github.com>
2025-04-17 18:34:12 +0100

    Add files via upload

--- stock_analysis_webapp.py
+++ stock_analysis_webapp.py
@@ -18,8 +18,8 @@ import joblib
 import numpy as np
 from textblob import TextBlob  # For basic sentiment analysis
 
-model = joblib.load("model/stock_predictor.pkl")
-label_encoder = joblib.load("model/label_encoder.pkl")
+model = joblib.load("stock_predictor.pkl")
+label_encoder = joblib.load("label_encoder.pkl")
 
 # Setup logging
 logging.basicConfig(level=logging.INFO, 
